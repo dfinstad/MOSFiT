32973632f4363e1522d215d284e173e856c68ec6
dfinstad <daniel.finstad@ligo.org>
2022-03-16 15:18:50 -0400

    update bns constraints

--- mosfit/modules/constraints/bns_constraints.py
+++ mosfit/modules/constraints/bns_constraints.py
@@ -1,6 +1,7 @@
 """Definitions for the `BNSConstraints` class."""
 import astropy.constants as c
 import numpy as np
+import logging
 
 from mosfit.constants import C_CGS, M_SUN_CGS, KM_CGS, G_CGS
 from mosfit.modules.constraints.constraint import Constraint
@@ -34,14 +35,19 @@ class BNSConstraints(Constraint):
         # Mass of lighter NS
         self._m2 = kwargs[self.key('M2')]
         if 'eos' in kwargs:
-            eosfile = "/home/daniel.finstad/projects/gw170817-eft-eos/eos_data/"
-            eosfile += "2nsat/{}.dat".format(int(kwargs[self.key('eos')]))
+            #eosfile = "/home/daniel.finstad/projects/gw170817-eft-eos/eos_data/"
+            #eosfile += "2nsat/{}.dat".format(int(kwargs[self.key('eos')]))
+            eosfile = "/home/daniel.finstad/projects/multimessenger_inference/"
+            eosfile += "NMMA/EOS/chiralEFT_MTOV/{}.dat".format(int(kwargs[self.key('eos')]))
             _, mdat, _ = np.loadtxt(eosfile, unpack=True)
             self._m_tov = np.max(mdat)
         else:
             self._m_tov = kwargs[self.key('Mtov')]
         self._r1 = kwargs[self.key('R1')]
         self._r2 = kwargs[self.key('R2')]
+        ckm = C_CGS / KM_CGS
+        self._vejecta_blue = kwargs[self.key('vejecta_blue')] / ckm
+        self._vejecta_red = kwargs[self.key('vejecta_red')] / ckm
 
         # Soft max/min, proportional to diff^2 and scaled to -100 for 0.1 Msun
         # 1
@@ -66,10 +72,29 @@ class BNSConstraints(Constraint):
             self._score_modifier -= (20. * (9-self._r2))**2
 
 
-        # 4
-        Mcaus = 1/2.82 * C_CGS**2 * self._r1 * KM_CGS / G_CGS / M_SUN_CGS
+        ## 4 enforce causality (should not be necessary for chiEFT runs)
+        #Mcaus = 1/2.82 * C_CGS**2 * self._r1 * KM_CGS / G_CGS / M_SUN_CGS
+        #if self._m_tov > Mcaus:
+        #    self._score_modifier -= (100. * (self._m_tov-Mcaus))**2
 
-        if self._m_tov > Mcaus:
-            self._score_modifier -= (100. * (self._m_tov-Mcaus))**2
+        # 5 ensure sane ejecta velocities
+        if self._vejecta_blue > 1.0:
+            self._score_modifier -= 1e10
+            #logging.warn("v_ejecta_blue is %s > 1! Modifying score by %s",
+            #             self._vejecta_blue, self._score_modifier)
+        elif self._vejecta_blue > 0.6:
+            self._score_modifier -= (1e3 * (self._vejecta_blue - 0.6)) ** 2.
+            #logging.warn("v_ejecta_blue is %s > 0.6! Modifying score by %s",
+            #             self._vejecta_blue, self._score_modifier)
+        if self._vejecta_red > 0.6:
+            self._score_modifier -= (1e3 * (self._vejecta_red - 0.6)) ** 2.
+        if self._vejecta_blue < 0.0:
+            self._score_modifier -= 1e10
+            #logging.warn("v_ejecta_blue is %s < 0! Modifying score by %s",
+            #             self._vejecta_blue, self._score_modifier)
+        elif self._vejecta_blue < 0.2:
+            self._score_modifier -= (1e3 * (0.2 - self._vejecta_blue)) ** 2.
+            #logging.warn("v_ejecta_blue is %s < 0.2! Modifying score by %s",
+            #             self._vejecta_blue, self._score_modifier)
 
         return {self.key('score_modifier'): self._score_modifier}
